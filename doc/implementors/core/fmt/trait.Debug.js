7a368f1452e6ef1c8fed1b30a04401a0311ddaef
Steven Fackler <sfackler@gmail.com>
2015-02-08 06:24:18 +0000

    rebuild pages at 7900e3cfc1cd7f0dbb6b92971606c94bc9cfa6ac

---
+++ doc/implementors/core/fmt/trait.Debug.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['log4rs'] = ["<a class='stability Stable' title='Stable'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html' title='core::fmt::Debug'>Debug</a> for <a class='struct' href='log4rs/loggers/struct.ConsoleLoggerMaker.html' title='log4rs::loggers::ConsoleLoggerMaker'>ConsoleLoggerMaker</a>","<a class='stability Stable' title='Stable'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html' title='core::fmt::Debug'>Debug</a> for <a class='struct' href='log4rs/loggers/struct.FileLoggerMaker.html' title='log4rs::loggers::FileLoggerMaker'>FileLoggerMaker</a>","<a class='stability Stable' title='Stable'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html' title='core::fmt::Debug'>Debug</a> for <a class='struct' href='log4rs/pattern/struct.PatternLogger.html' title='log4rs::pattern::PatternLogger'>PatternLogger</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
