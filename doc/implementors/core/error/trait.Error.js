72d179cbf0307ffaf3593b8a07dff5e14c4f3191
Steven Fackler <sfackler@gmail.com>
2015-03-03 18:27:14 +0000

    rebuild pages at 6265e97b14820fd9a90efd2b7e1316ae75a25061

---
+++ doc/implementors/core/error/trait.Error.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['log4rs'] = ["<a class='stability Unstable' title='Unstable: the exact API of this trait may change'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/error/trait.Error.html' title='core::error::Error'>Error</a> for <a class='struct' href='log4rs/pattern/struct.Error.html' title='log4rs::pattern::Error'>Error</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
---
+++ doc/implementors/core/fmt/trait.Display.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['log4rs'] = ["<a class='stability Stable' title='Stable'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/fmt/trait.Display.html' title='core::fmt::Display'>Display</a> for <a class='struct' href='log4rs/pattern/struct.Error.html' title='log4rs::pattern::Error'>Error</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
