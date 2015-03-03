f1c5682f7f5e881f73ca9138b39d3c995b50d405
Steven Fackler <sfackler@gmail.com>
2015-03-03 22:04:51 +0000

    rebuild pages at d175af9e9add7558b48f3df89ee151983c2f99bc

--- doc/implementors/core/cmp/trait.PartialEq.js
+++ doc/implementors/core/cmp/trait.PartialEq.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['log4rs'] = ["<a class='stability Stable' title='Stable'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/cmp/trait.PartialEq.html' title='core::cmp::PartialEq'>PartialEq</a> for <a class='struct' href='log4rs/config/struct.Error.html' title='log4rs::config::Error'>Error</a>",];
+implementors['log4rs'] = ["<a class='stability Stable' title='Stable'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/cmp/trait.PartialEq.html' title='core::cmp::PartialEq'>PartialEq</a> for <a class='enum' href='log4rs/config/enum.Error.html' title='log4rs::config::Error'>Error</a>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
