2dc4025d6b9b7ffe9a9027318dc1b7d90ba7a646
Steven Fackler <sfackler@gmail.com>
2015-03-14 20:15:14 +0000

    rebuild pages at c8dcc523e13cf43ec5aaa3416e0cd8cb30d1cbbe

---
+++ doc/log4rs/appender/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"struct":[["ConsoleAppender","An appender which logs to stdout."],["ConsoleAppenderBuilder","A builder for `ConsoleAppender`s."],["FileAppender","An appender which logs to a file."],["FileAppenderBuilder","A builder for `FileAppender`s."]]});
---
+++ doc/log4rs/pattern/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"struct":[["Error","An error parsing a `PatternLayout` pattern."],["PatternLayout","A formatter object for `LogRecord`s."]]});
