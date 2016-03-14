86145b658b6aa5a6c2ba84844cbe30c76a82b8dc
Steven Fackler <sfackler@gmail.com>
2016-03-13 22:39:03 -0700

    Adjust codegen to support multiple files

--- codegen/src/main.rs
+++ codegen/src/main.rs
@@ -1,14 +1,23 @@
 extern crate syntex;
 extern crate serde_codegen;
+extern crate walkdir;
 
-use std::path::Path;
+use walkdir::WalkDir;
 
 fn main() {
-    let src = Path::new("../src/file/serde.rs.in");
-    let dst = Path::new("../src/file/serde.rs");
+    for entry in WalkDir::new("../src").into_iter() {
+        let entry = entry.unwrap();
 
-    let mut registry = syntex::Registry::new();
-    serde_codegen::register(&mut registry);
+        if entry.path().extension().map(|e| e != "in").unwrap_or(true) {
+            continue;
+        }
 
-    registry.expand("", src, dst).unwrap();
+        let src = entry.path();
+        let dst = src.with_file_name(src.file_stem().unwrap());
+
+        let mut registry = syntex::Registry::new();
+        serde_codegen::register(&mut registry);
+
+        registry.expand("", src, &dst).unwrap();
+    }
 }
