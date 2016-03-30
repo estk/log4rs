f840daec3e185a0621638dfe908af2e1c661fa89
Steven Fackler <sfackler@gmail.com>
2016-03-29 20:58:07 -0700

    Release v0.4.0

--- src/lib.rs
+++ src/lib.rs
@@ -98,7 +98,7 @@
 //!       - requests
 //!     additive: false
 //! ```
-#![doc(html_root_url="https://sfackler.github.io/log4rs/doc/v0.3.3")]
+#![doc(html_root_url="https://sfackler.github.io/log4rs/doc/v0.4.0")]
 #![warn(missing_docs)]
 
 extern crate chrono;
