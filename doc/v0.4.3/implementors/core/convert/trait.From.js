178de0d0bbfd29423a7caea74402903b4507235d
Steven Fackler <sfackler@gmail.com>
2016-04-12 22:44:59 -0700

    Release v0.4.3

---
+++ doc/v0.4.3/implementors/core/convert/trait.From.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['log4rs'] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/convert/trait.From.html' title='core::convert::From'>From</a>&lt;<a class='struct' href='https://doc.rust-lang.org/log/log/struct.SetLoggerError.html' title='log::SetLoggerError'>SetLoggerError</a>&gt; for <a class='enum' href='log4rs/enum.Error.html' title='log4rs::Error'>Error</a>","impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/convert/trait.From.html' title='core::convert::From'>From</a>&lt;<a class='struct' href='https://doc.rust-lang.org/nightly/alloc/boxed/struct.Box.html' title='alloc::boxed::Box'>Box</a>&lt;<a class='trait' href='https://doc.rust-lang.org/nightly/std/error/trait.Error.html' title='std::error::Error'>Error</a>&gt;&gt; for <a class='enum' href='log4rs/enum.Error.html' title='log4rs::Error'>Error</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
