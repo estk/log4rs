d2c6c3fb2137d78c34a7e7881adf7264af9f8e02
Steven Fackler <sfackler@palantir.com>
2016-08-30 23:10:33 +0200

    Make delete roller config opaque

--- src/append/rolling_file/policy/compound/roll/delete/config.rs
+++ src/append/rolling_file/policy/compound/roll/delete/config.rs
@@ -8,7 +8,7 @@ const _IMPL_DESERIALIZE_FOR_DeleteRollerConfig: () =
              -> ::std::result::Result<DeleteRollerConfig, __D::Error> where
              __D: _serde::de::Deserializer {
                 #[allow(non_camel_case_types)]
-                enum __Field { }
+                enum __Field { __field0, }
                 impl _serde::de::Deserialize for __Field {
                     #[inline]
                     fn deserialize<__D>(deserializer: &mut __D)
@@ -24,6 +24,7 @@ const _IMPL_DESERIALIZE_FOR_DeleteRollerConfig: () =
                              -> ::std::result::Result<__Field, __E> where
                              __E: _serde::de::Error {
                                 match value {
+                                    0usize => { Ok(__Field::__field0) }
                                     _ =>
                                     Err(_serde::de::Error::invalid_value("expected a field")),
                                 }
@@ -32,6 +33,7 @@ const _IMPL_DESERIALIZE_FOR_DeleteRollerConfig: () =
                              -> ::std::result::Result<__Field, __E> where
                              __E: _serde::de::Error {
                                 match value {
+                                    "_p" => { Ok(__Field::__field0) }
                                     _ =>
                                     Err(_serde::de::Error::unknown_field(value)),
                                 }
@@ -40,6 +42,7 @@ const _IMPL_DESERIALIZE_FOR_DeleteRollerConfig: () =
                              -> ::std::result::Result<__Field, __E> where
                              __E: _serde::de::Error {
                                 match value {
+                                    b"_p" => { Ok(__Field::__field0) }
                                     _ => {
                                         let value =
                                             ::std::string::String::from_utf8_lossy(value);
@@ -61,8 +64,9 @@ const _IMPL_DESERIALIZE_FOR_DeleteRollerConfig: () =
                     fn visit_seq<__V>(&mut self, mut visitor: __V)
                      -> ::std::result::Result<DeleteRollerConfig, __V::Error>
                      where __V: _serde::de::SeqVisitor {
+                        let __field0 = ::std::default::Default::default();
                         try!(visitor . end (  ));
-                        Ok(DeleteRollerConfig{})
+                        Ok(DeleteRollerConfig{_p: __field0,})
                     }
                     #[inline]
                     fn visit_map<__V>(&mut self, mut visitor: __V)
@@ -71,13 +75,19 @@ const _IMPL_DESERIALIZE_FOR_DeleteRollerConfig: () =
                         while let Some(key) =
                                   try!(visitor . visit_key :: < __Field > (
                                        )) {
-                            match key { }
+                            match key {
+                                __Field::__field0 => {
+                                    try!(visitor . visit_value :: < _serde ::
+                                         de :: impls :: IgnoredAny > (  ));
+                                }
+                            }
                         }
                         try!(visitor . end (  ));
-                        Ok(DeleteRollerConfig{})
+                        Ok(DeleteRollerConfig{_p:
+                                                  ::std::default::Default::default(),})
                     }
                 }
-                const FIELDS: &'static [&'static str] = &[];
+                const FIELDS: &'static [&'static str] = &["_p"];
                 deserializer.deserialize_struct("DeleteRollerConfig", FIELDS,
                                                 __Visitor)
             }
@@ -85,4 +95,5 @@ const _IMPL_DESERIALIZE_FOR_DeleteRollerConfig: () =
     };
 /// Configuration for the delete roller.
 pub struct DeleteRollerConfig {
+    _p: (),
 }
