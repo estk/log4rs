08a80234696676663dc16f6ed42c6ea03e9e34a7
Steven Fackler <sfackler@gmail.com>
2017-04-04 19:35:10 -0700

    Release v0.6.3

--- src/lib.rs
+++ src/lib.rs
@@ -162,7 +162,7 @@
 //! # }
 //! # fn main() {}
 //! ```
-#![doc(html_root_url="https://docs.rs/log4rs/0.6.2")]
+#![doc(html_root_url="https://docs.rs/log4rs/0.6.3")]
 #![warn(missing_docs)]
 
 #[cfg(feature = "antidote")]
