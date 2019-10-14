2e5682a92d41dfcf7c5fd86f1d27174d3085ecba
Calvin Brown <calvinb085@icloud.com>
2019-10-14 06:55:09 -0500

    Adding JSON stdout logging example

---
+++ examples/json_logger.rs
@@ -0,0 +1,35 @@
+#[macro_use]
+extern crate log;
+
+use log::LevelFilter;
+use log4rs::append::console::ConsoleAppender;
+use log4rs::config::{Appender, Root};
+use log4rs::encode::json::JsonEncoder;
+
+
+//refresh_rate: 30 seconds
+//appenders:
+//    stdout:
+//        kind: console
+//        encoder:
+//            kind: json
+//root:
+//    level: info
+//    appenders:
+//        - stdout
+//The above YAML is the same as the programatically built logger below.
+
+fn main() {
+    let stdout: ConsoleAppender = ConsoleAppender::builder()
+        .encoder(Box::new(JsonEncoder::new()))
+        .build();
+    let log_config = log4rs::config::Config::builder()
+        .appender(Appender::builder().build("stdout", Box::new(stdout)))
+        .build(Root::builder().appender("stdout").build(LevelFilter::Info))
+        .unwrap();
+    log4rs::init_config(log_config).unwrap();
+
+    info!("Info log!");
+    warn!("Warn log with value {}", "test");
+    error!("ERROR!");
+}
