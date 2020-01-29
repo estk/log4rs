82fad0cb8a0662223435e6d879f7e74262d1a045
estk <esims89@gmail.com>
2020-01-29 10:02:00 -0800

    Add github workflows (#122)
    * Add github workflows
    
    * Docs and lints

--- cross-test.sh
+++
@@ -1,9 +0,0 @@
-# Test each feature in windows
-function main {
-    for feature in $(cargo read-manifest | jq -r '.features|keys|join("\n")'); do
-        echo building with feature "$feature"
-        cross test --target x86_64-pc-windows-gnu  --no-default-features --features "$feature"
-    done
-}
-
-main "$@"
