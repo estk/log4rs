d026c1a7b306b18a5f882466bd860b831e1c795f
Evan Simmons <esims89@gmail.com>
2020-02-23 13:23:38 -0800

    Rethink benches

--- benches/rotation.rs
+++ benches/rotation.rs
@@ -2,47 +2,61 @@
 
 use std::time::{Duration, Instant};
 
-use criterion::{criterion_group, criterion_main, Criterion};
 use lazy_static::lazy_static;
-use rayon::prelude::*;
 
 const K: u64 = 1024;
 const M: u64 = K * K;
-const FILE_SIZE: u64 = 10 * M;
+const FILE_SIZE: u64 = 100 * M;
 const FILE_COUNT: u32 = 10;
 
 lazy_static! {
     static ref HANDLE: log4rs::Handle =
         log4rs::init_config(mk_config(FILE_SIZE, FILE_COUNT)).unwrap();
-    static ref MSG: String = "0".repeat(80 * M as usize);
+    static ref MSG: String = "0".repeat(M as usize);
+}
+fn main() {
+    bench_find_anomalies();
 }
 
-fn log_group(c: &mut Criterion) {
+// This has been tuned on the assumption that the application will not log faster than we can gzip the files.
+// This should fail with just gzip feature enabled, and succeed with features = gzip,background_rotation enabled.
+fn bench_find_anomalies() {
+    use std::thread;
     lazy_static::initialize(&HANDLE);
 
-    let mut group = c.benchmark_group("Log");
-    group.sample_size(10);
-    group.measurement_time(Duration::SECOND * 30);
-    group.bench_function("log", |b| b.iter(a::write_log));
+    let mut samples = stats::Unsorted::default();
+    let mut mm = stats::MinMax::default();
+    let mut online = stats::OnlineStats::default();
+    let mut anomalies = vec![];
+    let iters = 1000;
+    for i in 1..iters {
+        thread::sleep(Duration::from_millis(5));
+        let now = Instant::now();
+        a::write_log();
+        let dur = now.elapsed();
+
+        if i > 100
+            && dur.as_micros() as u64 > (online.mean() + (online.stddev() * 10_f64)).round() as u64
+        {
+            anomalies.push(dur);
+        }
+
+        online.add(dur.as_micros());
+        samples.add(dur.as_micros());
+        mm.add(dur.as_micros());
+    }
+
+    if !anomalies.is_empty() {
+        use humantime::format_duration;
+        let min = format_duration(Duration::from_micros(*mm.min().unwrap() as u64));
+        let max = format_duration(Duration::from_micros(*mm.max().unwrap() as u64));
+        println!("min: {}\nmax: {}", min, max);
+        let median = format_duration(Duration::from_micros(samples.median().unwrap() as u64));
+        println!("median: {}", median);
+        println!("anomalies: {:?}", anomalies);
+    }
+    assert!(anomalies.is_empty(), "There should be no log anomalies");
 }
-
-fn log2_group(c: &mut Criterion) {
-    lazy_static::initialize(&HANDLE);
-
-    let mut group = c.benchmark_group("Parallel Log");
-    group.sample_size(10);
-    group.measurement_time(Duration::SECOND * 30);
-    group.bench_function("log", |b| {
-        b.iter_custom(|iter| {
-            let now = Instant::now();
-            (0..iter).into_par_iter().for_each(|_| a::write_log());
-            now.elapsed()
-        })
-    });
-}
-
-criterion_group!(benches, log_group, log2_group);
-criterion_main!(benches);
 
 mod a {
     pub fn write_log() {
@@ -51,6 +65,11 @@ mod a {
 }
 
 fn mk_config(file_size: u64, file_count: u32) -> log4rs::config::Config {
+    let logdir = tempfile::tempdir().unwrap();
+    let log_path = logdir.path();
+    let log_pattern = log_path.join("log.log");
+    let roll_pattern = format!("{}/{}", log_path.to_string_lossy(), "log.{}.gz");
+
     use log::LevelFilter;
     use log4rs::{
         append::rolling_file::{policy, RollingFileAppender},
@@ -59,14 +78,14 @@ fn mk_config(file_size: u64, file_count: u32) -> log4rs::config::Config {
     };
     let trigger = policy::compound::trigger::size::SizeTrigger::new(file_size);
     let roller = policy::compound::roll::fixed_window::FixedWindowRoller::builder()
-        .build("logs/log.{}.gz", file_count)
+        .build(&roll_pattern, file_count)
         .unwrap();
     let policy = policy::compound::CompoundPolicy::new(Box::new(trigger), Box::new(roller));
     let file = RollingFileAppender::builder()
         .encoder(Box::new(PatternEncoder::new(
             "{d(%Y-%m-%d %H:%M:%S.%3f %Z)} {l} [{t} - {T}] {m}{n}",
         )))
-        .build("logs/log.log", Box::new(policy))
+        .build(&log_pattern, Box::new(policy))
         .unwrap();
 
     Config::builder()
