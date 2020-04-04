6fc14c6887c2b511d71d0955ce710b29a8008ddd
Evan Simmons <esims89@gmail.com>
2020-04-04 01:45:00 -0700

    Commented code

--- src/lib.rs
+++ src/lib.rs
@@ -182,9 +182,9 @@
 //!
 //! For more examples see the (examples)[https://github.com/estk/log4rs/tree/master/examples] in the source.
 //!
-// #![deny(clippy::all)]
 #![allow(where_clauses_object_safety)]
 #![warn(missing_docs)]
+
 use arc_swap::ArcSwap;
 use fnv::FnvHasher;
 use log::{Level, LevelFilter, Metadata, Record, SetLoggerError};
