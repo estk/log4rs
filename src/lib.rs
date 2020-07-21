19e403297f4fbcf6567d73eb9dd9519ef3b76e76
Evan Simmons <esims89@gmail.com>
2020-07-21 16:48:10 -0700

    clippy

--- src/lib.rs
+++ src/lib.rs
@@ -183,7 +183,7 @@
 //! For more examples see the (examples)[https://github.com/estk/log4rs/tree/master/examples] in the source.
 //!
 
-#![allow(where_clauses_object_safety)]
+#![allow(where_clauses_object_safety, clippy::manual_non_exhaustive)]
 #![warn(missing_docs)]
 
 use arc_swap::ArcSwap;
