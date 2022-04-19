2fba125d62e9a22852733349e42a2c937d6f08fa
Vivek Bharath Akupatni <apatniv@gmail.com>
2022-04-19 14:43:01 -0400

    Add example of using compile time config (#227)
    * Add example of using compile time config
    
    * Fix Linter issues
    
    * Address Lint and Compilation Issue. Fixed Conflicts

---
+++ examples/compile_time_config.rs
@@ -0,0 +1,13 @@
+use log::{error, info, trace};
+use log4rs;
+use serde_yaml;
+
+fn main() {
+    let config_str = include_str!("sample_config.yml");
+    let config = serde_yaml::from_str(config_str).unwrap();
+    log4rs::init_raw_config(config).unwrap();
+
+    info!("Goes to console");
+    error!("Goes to console");
+    trace!("Doesn't go to console as it is filtered out");
+}
