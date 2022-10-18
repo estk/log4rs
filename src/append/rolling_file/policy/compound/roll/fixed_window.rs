4a7da342a5f8fb2aa140c6f802dd8f61e1948680
Caleb Hill <foamcactus@protonmail.com>
2022-10-18 10:29:49 -0500

    Update rotate to take into accound env vars
    This commit updates the rotate function to expand any environmental
    variable sections in the pattern argument.

--- src/append/rolling_file/policy/compound/roll/fixed_window.rs
+++ src/append/rolling_file/policy/compound/roll/fixed_window.rs
@@ -12,6 +12,7 @@ use std::{
     path::{Path, PathBuf},
 };
 
+use crate::append::env_util::expand_env_vars;
 use crate::append::rolling_file::policy::compound::roll::Roll;
 #[cfg(feature = "config_parsing")]
 use crate::config::{Deserialize, Deserializers};
@@ -199,7 +200,7 @@ fn rotate(
     count: u32,
     file: PathBuf,
 ) -> io::Result<()> {
-    let dst_0 = pattern.replace("{}", &base.to_string());
+    let dst_0 = expand_env_vars(pattern.clone().replace("{}", &base.to_string()));
 
     if let Some(parent) = Path::new(&dst_0).parent() {
         fs::create_dir_all(parent)?;
@@ -207,14 +208,17 @@ fn rotate(
 
     // In the common case, all of the archived files will be in the same
     // directory, so avoid extra filesystem calls in that case.
-    let parent_varies = match (Path::new(&dst_0).parent(), Path::new(&pattern).parent()) {
+    let parent_varies = match (
+        Path::new(&dst_0).parent(),
+        Path::new(&expand_env_vars(&pattern)).parent(),
+    ) {
         (Some(a), Some(b)) => a != b,
         _ => false, // Only case that can actually happen is (None, None)
     };
 
     for i in (base..base + count - 1).rev() {
-        let src = pattern.replace("{}", &i.to_string());
-        let dst = pattern.replace("{}", &(i + 1).to_string());
+        let src = expand_env_vars(pattern.replace("{}", &i.to_string()));
+        let dst = expand_env_vars(pattern.replace("{}", &(i + 1).to_string()));
 
         if parent_varies {
             if let Some(parent) = Path::new(&dst).parent() {
