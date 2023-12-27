c7270f7b1a4c1e3bcd97193f34f90ae9c418aa37
One <43485962+c-git@users.noreply.github.com>
2023-12-27 09:30:13 -0500

    Move configuration values out of main to top

--- examples/log_to_file_with_rolling.rs
+++ examples/log_to_file_with_rolling.rs
@@ -1,3 +1,30 @@
+//! Example showing how to use logging with a rolling trigger based on size
+//!
+//! NB: The size used in the example is intentionally small so multiple file
+//! will be created in the 2 seconds that the example is set to run and is not
+//! intended for practical for usage
+
+/// This is the size at which a new file should be created. For the demo it is
+/// set to 2KB which is very small and only for demo purposes
+const TRIGGER_FILE_SIZE: u64 = 2 * 1024;
+
+/// Delay between log messages for demo purposes
+const TIME_BETWEEN_LOG_MESSAGES: Duration = Duration::from_millis(10);
+
+/// Number of archive log files to keep
+const LOG_FILE_COUNT: u32 = 3;
+
+/// Time demo is set to run for (Set to be long enough for multiple files to be created)
+const RUN_TIME: Duration = Duration::from_secs(2);
+
+/// Location where logs will be written to
+const FILE_PATH: &str = "/tmp/foo.log";
+
+/// Location where log archives will be moved to
+/// For Pattern info See:
+///     https://docs.rs/log4rs/*/log4rs/append/rolling_file/policy/compound/roll/fixed_window/struct.FixedWindowRollerBuilder.html#method.build
+const ARCHIVE_PATTERN: &str = "/tmp/archive/foo.{}.log";
+
 use std::thread::sleep;
 use std::time::{Duration, Instant};
 
@@ -16,18 +43,15 @@ use log4rs::{
 
 fn main() -> Result<(), SetLoggerError> {
     let level = log::LevelFilter::Info;
-    let file_path = "/tmp/foo.log";
-    let archive_pattern = "/tmp/archive/foo.{}.log";
-    // Pattern: https://docs.rs/log4rs/*/log4rs/append/rolling_file/policy/compound/roll/fixed_window/struct.FixedWindowRollerBuilder.html#method.build
 
     // Build a stderr logger.
     let stderr = ConsoleAppender::builder().target(Target::Stderr).build();
 
     // Create a policy to use with the file logging
-    let trigger = SizeTrigger::new(2048); // 2kb (2 * 1024)
+    let trigger = SizeTrigger::new(TRIGGER_FILE_SIZE);
     let roller = FixedWindowRoller::builder()
         .base(0) // Default Value (line not needed unless you want to change from 0 (only here for demo purposes)
-        .build(archive_pattern, 3) // Roll based on pattern and max 3 archive files
+        .build(ARCHIVE_PATTERN, LOG_FILE_COUNT) // Roll based on pattern and max 3 archive files
         .unwrap();
     let policy = CompoundPolicy::new(Box::new(trigger), Box::new(roller));
 
@@ -35,7 +59,7 @@ fn main() -> Result<(), SetLoggerError> {
     let logfile = log4rs::append::rolling_file::RollingFileAppender::builder()
         // Pattern: https://docs.rs/log4rs/*/log4rs/encode/pattern/index.html
         .encoder(Box::new(PatternEncoder::new("{l} - {m}\n")))
-        .build(file_path, Box::new(policy))
+        .build(FILE_PATH, Box::new(policy))
         .unwrap();
 
     // Log Trace level output to file where trace is the default level
@@ -69,15 +93,13 @@ fn main() -> Result<(), SetLoggerError> {
 
     // Generate some log messages to trigger rolling
     let instant = Instant::now();
-    let sleep_time = Duration::from_millis(10);
-    let run_time = Duration::from_secs(2);
-    while instant.elapsed() < run_time {
+    while instant.elapsed() < RUN_TIME {
         info!("Running for {:?}", instant.elapsed());
-        sleep(sleep_time);
+        sleep(TIME_BETWEEN_LOG_MESSAGES);
     }
     info!(
         "See '{}' for log and '{}' for archived logs",
-        file_path, archive_pattern
+        FILE_PATH, ARCHIVE_PATTERN
     );
 
     Ok(())
